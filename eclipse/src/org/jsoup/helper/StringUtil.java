c47819c39d397fdc9f4f20d65e4ab6c99b57db52
Antanas Sinica <antanas.sinica@gmail.com>
2011-06-06 19:15:16 +0300

    Removing unneeded bin files

--- eclipse/src/org/jsoup/helper/StringUtil.java
+++ eclipse/src/org/jsoup/helper/StringUtil.java
@@ -16,7 +16,7 @@ public final class StringUtil {
      * @param sep string to place between strings
      * @return joined string
      */
-    public static String join(Collection strings, String sep) {
+    public static String join(Collection<?> strings, String sep) {
         return join(strings.iterator(), sep);
     }
 
